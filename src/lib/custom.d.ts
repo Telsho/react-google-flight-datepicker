72723d0139a66ecb8c8b22e7f0bd8d7a43b96455
Elias Thouant <elias.thouant@tmceurope.com>
2025-01-23 14:32:03 +0100

    fix: dev tools

--- src/lib/custom.d.ts
+++ src/lib/custom.d.ts
@@ -1,4 +1,5 @@
-declare module "*.svg" {
-    const content: any;
-    export default content;
-  }
+declare module '*.svg' {
+  import React from 'react';
+  const content: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
---
+++ webpack.dev.js
@@ -0,0 +1,94 @@
+// webpack.dev.js (in project root)
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/dev/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dev-dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dev-dist'),
+    },
+    hot: true,
+    port: 3011,
+    open: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+    alias: {
+      '@telsho/react-google-flight-datepicker': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.svg$/,
+        oneOf: [
+          {
+            issuer: /\.[jt]sx?$/,
+            use: [
+              {
+                loader: '@svgr/webpack',
+                options: {
+                  svgo: false,
+                  titleProp: true,
+                }
+              }
+            ]
+          },
+          {
+            type: 'asset/resource',
+            generator: {
+              filename: 'static/media/[name].[hash][ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>React Google Flight Datepicker</title>
+        </head>
+        <body>
+          <div id="root"></div>
+        </body>
+      </html>
+      `,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
+};
