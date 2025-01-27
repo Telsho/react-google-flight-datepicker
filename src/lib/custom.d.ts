6fbbfe3d447f6b16cd4207e827f1593a08864223
Elias Thouant <elias.thouant@tmceurope.com>
2025-01-27 10:46:55 +0100

    fix: css + svg loading

--- src/lib/custom.d.ts
+++ src/lib/custom.d.ts
@@ -1,5 +1,15 @@
 declare module '*.svg' {
   import React from 'react';
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const content: string;
   export default content;
 }
